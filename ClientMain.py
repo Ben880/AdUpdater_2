6b71e99a007a7670eff36f990a808a67b0633455
Ben880 <Ben880@users.noreply.github.com>
2021-07-04 10:58:38 -0400

    Test Networking
    Networking test before rewrite

--- ClientMain.py
+++ ClientMain.py
@@ -9,37 +9,47 @@
 # ======================================================================================================================
 # =============================== imports ==============================================================================
 # ======================================================================================================================
+import json
 import socket
 import psutil
 import subprocess
 import os.path
 import time
-import shutil
-import datetime
+from SharedAssets import Tools
 import os
 import sys
 from SharedAssets.Config import Config as Config
+
 # ======================================================================================================================
 # =============================== create vars ==========================================================================
 # ======================================================================================================================
+from SharedAssets.DeltaTime import DeltaTime
+
 cwd = os.getcwd()
 dir_path = os.path.dirname(os.path.realpath(__file__))
-cfg = Config(configFile="clientcfg.json", fileDir=os.path.join(dir_path, "config"))
-cfg.load()
+pid = str(os.getpid())
 # ======================================================================================================================
 # =============================== load cfg vars ========================================================================
 # ======================================================================================================================
+cfg = Config(configFile="clientcfg.json", fileDir=os.path.join(dir_path, "Config"))
+cfg.load()
+projectName = cfg.getVal("project_name")
 powerPoint = cfg.getVal("power_point")
 shortcutFile = cfg.getVal("power_point_shortcut")
 verificationFile = cfg.getVal("verification_file")
 printKeepAlive = cfg.getVal("print_keep_alive")
 updateTimer = cfg.getVal("update_timer")
 process_name = cfg.getVal("process_name")
-localFolder = cfg.getVal("local_folder")
-pidfile = cfg.getVal("pid_file")
+localPowerpoints = cwd + "/" + cfg.getVal("local_powerpoints")
+pidFile = cfg.getVal("pid_file")
+respect_pid = cfg.getVal("respect_pid")
+client_name = cfg.getVal("client_name")
+
+
 # ======================================================================================================================
 # =============================== functions ============================================================================
 # ======================================================================================================================
+# get pids with process_name (cfg file)
 def getPID():
     pid = list()
     for proc in psutil.process_iter():
@@ -48,6 +58,7 @@ def getPID():
     return pid
 
 
+# get stats of pid list passed
 def getPIDStats(pid: list):
     stats = list()
     for i in pid:
@@ -56,102 +67,101 @@ def getPIDStats(pid: list):
         stats.append((i, s))
     return stats
 
+
+def stop_shows():
+    pids = getPID()
+    if len(pids) > 0:
+        Tools.format_print(f"found {len(pids)} power points to end")
+    else:
+        Tools.format_print("No power point process exists to end")
+    for p in pids:
+        Tools.format_print(f"Ending power point with PID:{p}")
+        subprocess.call(f'taskkill /PID {p} /f', shell=True)
+
+
+def delete_powerpoint():
+    if os.path.isfile(localPowerpoints + powerPoint):
+        Tools.format_print("Removing old power point")
+        while os.path.isfile(localPowerpoints + powerPoint):
+            try:
+                os.remove(localPowerpoints + powerPoint)
+            except PermissionError:
+                Tools.format_print("Cannot remove old file, trying again in 50ms.")
+                time.sleep(.005)
+    else:
+        Tools.format_print(f"No such file {powerPoint}")
+
+
+
 def fetch_file():
-    # kill running power point
-    subprocess.call('taskkill /IM PPTVIEW.EXE /f', shell=True)
-    # while local file exists try delete local file
-    while os.path.isfile(localFolder + powerPoint):
-        try:
-            os.remove(localFolder + powerPoint)
-        except PermissionError:
-            printF("Cannot remove old file, trying again in 100ms.")
-            time.sleep(.01)
+    Tools.format_print("Fetching new power point")
+    stop_shows()
+    delete_powerpoint()
     # grab remote file and start power point then verify update
-    shutil.copy(remoteFolder + powerPoint, localFolder + powerPoint)
-    subprocess.call("start " + localFolder + startFile, shell=True)
+    # TODO: grab remote file
+    start_show()
     updateVerification()
+    Tools.format_print("Fetch file complete")
 
 
-def printF(s):
-    global printCheckForUpdates
-    print(dt() + "> " + s)
-    printCheckForUpdates = True
-
-
-def dt():
-    return datetime.datetime.fromtimestamp(time.time()).strftime('%m-%d-%Y %H:%M:%S')
+def start_show():
+    Tools.format_print("Starting the power point")
+    if os.path.isfile(localPowerpoints + powerPoint):
+        subprocess.call("start " + shortcutFile, shell=True)
+    else:
+        Tools.format_print("No show to start")
 
 
 def updateVerification():
-    f = open(localFolder + verificationFile, "w")
-    f.write(dt())
+    Tools.format_print("Updating verification")
+    f = open(os.path.join(localPowerpoints, verificationFile), "w")
+    f.write(Tools.date_time())
     f.close()
     # TODO: logic for updating main server
 
-def oldcode():
-    PID = getPID()
-    sts = getPIDStats(PID)
-    for e in sts:
-        print(e)
+def send_packet(server, packet):
+    server.sendall(bytes(json.dumps(packet), "utf-8"))
+    rpc = packet["rpc"]
+    Tools.format_print(f"sent rpc:{rpc}")
+
 
 def MAINLOOP():
-    EXIT_STATUS = -1
-    while EXIT_STATUS == 1:
-        # ========== File Check ================
-        # If no local file or remote is newer fetch from remote
-        if os.path.isfile(remoteFolder + powerPoint):
-            if (not os.path.isfile(localFolder + powerPoint)) or os.path.getmtime(localFolder + powerPoint) <= os.path.getmtime(remoteFolder + powerPoint):
-                printF("fetching file")
-                fetch_file()
-        else:
-            printF("Error: remote path invalid")
-        # ========== Loop Notifier ================
-        # If other notifications were printed re-print looping
-        if printCheckForUpdates:
-            printF("checking for updates every " + str(updateTimer) + " minutes")
-            printCheckForUpdates = False
-        time.sleep(updateTimer*60)
-    print(f"Main loop finished with exit code: {EXIT_STATUS}")
+    if Tools.is_dir_mk(localPowerpoints):
+        if not os.path.isfile(localPowerpoints + powerPoint):
+            Tools.format_print("Power point does not exist")
+    EXIT_STATUS = 1
     HOST = '127.0.0.1'  # The server's hostname or IP address
-    PORT = 65432        # The port used by the server
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(b'Hello, world')
-        data = s.recv(1024)
-
-    print('Received', repr(data))
+    PORT = 65432  # The port used by the server
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((HOST, PORT))
+    send_packet(server, {"rpc": "CONNECT", "data": client_name})
+    while EXIT_STATUS == 1:
+        data = server.recv(1024)
+        fetch_file()
+    Tools.format_print(f"Main loop finished with exit code: {EXIT_STATUS}")
+    send_packet(server, {"rpc": "CLOSE_CONNECTION", "data": client_name})
+    server.close()
+    print('Received', str(data, "utf-8"))
 
 
 # ======================================================================================================================
 # =============================== main logic ===========================================================================
 # ======================================================================================================================
-pid = str(os.getpid())
-if os.path.isfile(pidfile):
-    print("pid file already exists, exiting")
+# if pid file exists print and close
+if respect_pid and os.path.isfile(pidFile):
+    Tools.format_print("pid file already exists, exiting")
     time.sleep(cfg.getVal("exit_timer"))
     sys.exit()
+# else create pid and write pid
 else:
-    print(f"creating pid file: {pidfile}")
-    open(pidfile, 'w').write(pid)
+    Tools.format_print(f"creating pid file: {pidFile}")
+    open(pidFile, 'w').write(pid)
+# try main loop and finally remove pid file
 try:
-    projectName = cfg.getVal("project_name")
-    print(f"Running {projectName}- Client")
+    Tools.format_print(f"Running {projectName}:Client")
     MAINLOOP()
 finally:
-    print(f"EOF, removing pid file: {pidfile}")
-    os.unlink(os.path.join(dir_path, pidfile))
-    print(f"exiting")
+    Tools.format_print("Exiting")
+    Tools.format_print(f"Removing pid file: {pidFile}")
+    os.unlink(os.path.join(dir_path, pidFile))
     time.sleep(cfg.getVal("exit_timer"))
-
-
-
-
-
-
-
-
-
-
-
-
--- ServerMain.py
+++ ServerMain.py
@@ -9,33 +9,86 @@
 # ======================================================================================================================
 # =============================== imports ==============================================================================
 # ======================================================================================================================
+import json
 import os
-import socket
+from SharedAssets import Tools
+import select
+import threading
+import socketserver
+
+from SharedAssets.ClientList import ClientList, ClientInfo
 from SharedAssets.Config import Config as Config
+
 # ======================================================================================================================
 # =============================== create vars ==========================================================================
 # ======================================================================================================================
 cwd = os.getcwd()
 dir_path = os.path.dirname(os.path.realpath(__file__))
-cfg = Config(configFile="servercfg.json", fileDir=os.path.join(dir_path, "config"))
+cfg = Config(configFile="servercfg.json", fileDir=os.path.join(dir_path, "Config"))
 cfg.load()
 # ======================================================================================================================
 # =============================== load cfg vars ========================================================================
 # ======================================================================================================================
 host = cfg.getVal("host")
 port = cfg.getVal("port") # Port to listen on (non-privileged ports are > 1023)
+projectName = cfg.getVal("project_name")
+client_list = ClientList()
+
+
+# ======================================================================================================================
+# =============================== func logic ===========================================================================
+# ======================================================================================================================
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+    connect = True
+
+    def setup(self):
+        Tools.format_print("ThreadedTCPRequestHandler Started")
+        client = ClientInfo("None", self)
+        client_list.add_client(client)
+
+    def handle(self):
+        while self.connect:
+            r,w,e = select.select([self.request],[],[],0.01)
+            for rs in r:
+                if rs == self.request:
+                    packet = str(self.request.recv(1024), "utf-8")
+                    json_data = json.loads(packet)
+                    rpc_name = json_data["rpc"]
+                    rpc_data = json_data["data"]
+                    if rpc_name == "CLOSE_CONNECTION":
+                        return
+                    elif rpc_name == "CONNECT":
+                        Tools.format_print(f"Client connected name:{rpc_data}")
+                        client_info: ClientInfo = client_list.get_client(self)
+                        client_info.set_name(rpc_data)
+
+    def send(self, packet):
+        server.sen
+
+    def finish(self):
+        Tools.format_print("ThreadedTCPRequestHandler Ended")
+
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+
+def passtime():
+    pass
+
 
 # ======================================================================================================================
 # =============================== main logic ===========================================================================
 # ======================================================================================================================
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((host, port))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+Tools.format_print(f"Running {projectName}:Client")
+# Create threaded server
+server = ThreadedTCPServer((host, port), ThreadedTCPRequestHandler)
+ip, port = server.server_address
+server_thread = threading.Thread(target=server.serve_forever)
+# start server
+server_thread.daemon = True
+server_thread.start()
+Tools.format_print("Server Started")
+while server_thread:
+    passtime()
+
